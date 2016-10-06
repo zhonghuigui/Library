f4c16a4201f904f05b483d0e733f21775426650f
SK-SoulKing <2468639494@qq.com>
2016-10-06 16:32:28 +0800

    sk

---
+++ library-web/src/main/java/com.hzit/controller/OrderController.java
@@ -0,0 +1,7 @@
+package com.hzit.controller;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+public class OrderController {
+}
---
+++ library-web/src/main/java/com.hzit/services/BookService.java
@@ -0,0 +1,7 @@
+package com.hzit.services;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+public interface BookService {
+}
