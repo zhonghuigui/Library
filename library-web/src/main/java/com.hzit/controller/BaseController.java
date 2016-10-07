69abb93ecc9e89ae37e4f35492403e3f9450addf
zhonghuigui <515268535@qq.com>
2016-10-07 20:13:36 +0800

    zhonghuigui

--- library-web/src/main/java/com.hzit/controller/BaseController.java
+++ library-web/src/main/java/com.hzit/controller/BaseController.java
@@ -10,7 +10,7 @@ public class BaseController extends WebMvcConfigurerAdapter {
           @Override
            public void addInterceptors(InterceptorRegistry registry) {
                     //利用拦截器注册一个对象，给请求指定一个拦截的对象，并且指定拦截的路径。
-                 //  registry.addInterceptor(new Checklogin()).addPathPatterns("/controller/**");
+                 //  registry.addInterceptor(new CheckUserInterceptor()).addPathPatterns("/controller/**");
                    super.addInterceptors(registry);
                }
 
